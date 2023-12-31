166c1c247a1b0705bae45d27380aaa62fbb6f921
Merrick Richers <merrickrichers@gmail.com>
2023-12-30 22:27:53 -0800

    added drawing functionality
    uses event listeners to track the position of the mouse when it
    is pressed down. Position is tracked relative to the canvas.
    It works, but not very well. The pen is too small, and the marks
    are sporadic. There is also an offset from the cursor, implying that
    the coordinates are not being calculated correctly.

--- script.js
+++ script.js
@@ -20,6 +20,67 @@ image1.addEventListener("load", function() {
     // from top to bottom, left to right.
     // in format [R, G, B, A, R2, G2, B2, A2, R3... 
     const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const scannedData = scannedImage.data;
     // prints the array to inspect element console
     console.log(scannedImage);
-})
+
+    // creates mouse dictionary with x y coordinates as well as a radius
+    const mouse = {
+        x: null,
+        y: null,
+        state: "up"
+        //radius: 20
+    }
+
+    // adapted from https://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element
+    function getCursorPosition(canvas, event) {
+        // gets the bounding rectangle of the canvas
+        const canvasRectangle = canvas.getBoundingClientRect();
+        // calculates the x/y coordinates relative to the canvas' coordinates
+        mouse.x = event.x - canvasRectangle.left;
+        mouse.y = event.y - canvasRectangle.top;
+        // prints to console
+        console.log(mouse.x, mouse.y);
+    }
+
+    function drawInk(event) { 
+        col = Math.round(mouse.x)
+        row = Math.round(mouse.y)*canvas.width -1
+        index = (col+row)*4
+        scannedData[index] = 0;
+        scannedData[index + 1] = 0;
+        scannedData[index + 2] = 0;
+        scannedData[index + 3] = 255;
+        scannedImage.data = scannedData;
+        ctx.putImageData(scannedImage, 0, 0);
+    }
+
+    // keeps track of the mouse state
+    window.addEventListener('mousedown', function(event) {
+        mouse.state = "down"
+        console.log(mouse.state)
+    })
+    window.addEventListener('mouseup', function(event) {
+        mouse.state = "up"
+        console.log(mouse.state)
+    })
+
+    // records path of pen stroke
+    canvas.addEventListener('mousemove', function(event) {
+        if (mouse.state == "down"){
+            getCursorPosition(canvas, event)
+            drawInk(event)
+        }
+    })
+
+});
+
+
+// gets the mouse position as x and y values
+/*
+window.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    console.log(mouse.x, mouse.y);
+});
+*/
