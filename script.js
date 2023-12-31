ae3d3e5ce38f37af63a0af7d1c1042a58648eb96
Merrick Richers <merrickrichers@gmail.com>
2023-12-31 10:12:25 -0800

    implemented line algorithm to draw mouse path
    Bresenham's algorithm was used to connect the mouse positions, which
    are not continuous. This makes the pen behave more realistically, as
    it now makes continous strokes.
    The algorithm implementation is slow and I do not fully understand
    how it works. array sizes should be preallocated.

--- script.js
+++ script.js
@@ -19,12 +19,13 @@ const mouse = {
     x: null,
     y: null,
     // these store the previous values of x and y to apply line alogrithms.
-    Pastx: null,
-    Pasty: null,
+    pastX: null,
+    pastY: null,
     state: "up",
     radius: 10
 }
 
+// a circle rendering algorithm of my own design
 image1.addEventListener("load", function() {
     // creates an array representing the shape of the pen
     function makeCirclePen(radius) {
@@ -73,11 +74,11 @@ image1.addEventListener("load", function() {
         scannedData[index + 3] = a;
     }
     
-    // draws mark on image
-    function drawInk(event) { 
+    // draws mark on image at specified position
+    function drawInk(event, X, Y) { 
         for (let coordinate = 0; coordinate < pen.length; coordinate++) {
-            x = mouse.x + pen[coordinate][0]
-            y = mouse.y + pen[coordinate][1]
+            x = X + pen[coordinate][0]
+            y = Y + pen[coordinate][1]
             // prevents edge pixels from bleeding onto opposite side of canvas.
             if (x < canvas.width && x > 0) {
             index = convertXYtoIndex(x,y)
@@ -88,10 +89,79 @@ image1.addEventListener("load", function() {
         ctx.putImageData(scannedImage, 0, 0);
     }
 
-    // this is an implementation of bresenham's line algorithm
-    // from https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm#
-    function bresenhamLine(x1, y1, x2, y2) {
+    // executes bresenham's algorithm for slopes in between -1 and 1
+    function smallSlope(x0, y0, x1, y1) {
+        path = [] // initializes array to store values
 
+        dx = x1 - x0;
+        dy = y1 - y0;
+        yi = 1
+        if (dy < 0) {
+            yi *= -1
+            dy *= -1
+        }
+        d = 2*dy - dx
+        y = y0
+        
+        for (let x = x0; x < x1; x++) {
+            path.push([x, y])
+            if (d > 0) {
+                y += yi
+                d += 2 * (dy - dx)
+            }
+            else {
+                d += 2*dy
+            }
+        }
+
+        return path
+    }
+    // executes bresenham's algorithm for slopes greater than 1 or less than -1.
+    function steepSlope(x0, y0, x1, y1) {
+        path = []
+
+        dx = x1 - x0;
+        dy = y1 - y0;
+        xi = 1
+        if (dx < 0) {
+            xi *= -1
+            dx *= -1
+        }
+        d = 2*dx - dy
+        x = x0
+        
+        for (let y = y0; y < y1; y++) {
+            path.push([x, y])
+            if (d > 0) {
+                x += xi
+                d += 2 * (dx - dy)
+            }
+            else {
+                d += 2*dx
+            }
+        }
+
+        return path
+    }
+    // this is an implementation of bresenham's line algorithm
+    // adapted from https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm#
+    function bresenhamLine(x0, y0, x1, y1) {
+        if (Math.abs(y1 - y0) < Math.abs(x1 - x0)) {
+            if (x0 < x1) { // checks if line goes backwards or forwards
+                return smallSlope(x0,y0,x1,y1)
+            }
+            else {
+                return smallSlope(x1,y1,x0,y0)
+            }
+        }
+        else {
+            if (y0 < y1) { // checks if line goes backwards or forwards
+                return steepSlope(x0,y0,x1,y1)
+            }
+            else {
+                return steepSlope(x1,y1,x0,y0)
+            }
+        }
     }
 
     // renders image
@@ -109,7 +179,11 @@ image1.addEventListener("load", function() {
 
     // keeps track of the mouse state
     window.addEventListener('mousedown', function(event) {
+        getCursorPosition(canvas, event)
         mouse.state = "down"
+        // initialize past x and y
+        mouse.pastX = mouse.x
+        mouse.pastY = mouse.y
         console.log(mouse.state)
     })
     window.addEventListener('mouseup', function(event) {
@@ -121,7 +195,12 @@ image1.addEventListener("load", function() {
     canvas.addEventListener('mousemove', function(event) {
         if (mouse.state == "down"){
             getCursorPosition(canvas, event)
-            drawInk(event)
+            path = bresenhamLine(mouse.pastX, mouse.pastY, mouse.x, mouse.y)
+            for (let i = 0; i < path.length; i++) {
+                drawInk(event, path[i][0], path[i][1])
+            }
+            mouse.pastX = mouse.x
+            mouse.pastY = mouse.y
         }
     })
 
